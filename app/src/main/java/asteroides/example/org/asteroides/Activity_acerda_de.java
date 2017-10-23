a1b44a22c9d950f355876172e3ea47ad22bf707e
Shinigami <denshaotoko@outlook.com>
2017-10-23 17:08:27 +0200

    commit asteroides2 con tema modificado y un listener en el boton acerca de...

---
+++ app/src/main/java/asteroides/example/org/asteroides/Activity_acerda_de.java
@@ -0,0 +1,13 @@
+package asteroides.example.org.asteroides;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_acerda_de extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acerda_de);
+    }
+}
--- app/src/main/java/asteroides/example/org/asteroides/MainActivity.java
+++ app/src/main/java/asteroides/example/org/asteroides/MainActivity.java
@@ -1,13 +1,34 @@
 package asteroides.example.org.asteroides;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button bAcercaDe;
+
+    public void lanzarAcercaDe(View view){
+        Intent i = new Intent(this, Activity_acerda_de.class);
+        startActivity(i);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bAcercaDe = (Button) findViewById(R.id.button4);
+        bAcercaDe.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                lanzarAcercaDe(null);
+            }
+        });
+
     }
+
 }
+
