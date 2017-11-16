01bc6b4ab4ee37673cd79ccb3f552436932bf2ca
ShinigamiOnline <denshaotoko@outlook.com>
2017-11-16 11:11:45 +0100

    commit commit commit

--- app/src/main/java/asteroides/example/org/asteroides/MainActivity.java
+++ app/src/main/java/asteroides/example/org/asteroides/MainActivity.java
@@ -3,6 +3,7 @@ package asteroides.example.org.asteroides;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 
@@ -57,6 +58,12 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 
     }
