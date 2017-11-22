b8be5bf25b8165fb22b3fea9222cad3425cb470f
Shinigami <denshaotoko@outlook.com>
2017-11-22 21:52:40 +0100

    commit Gradient Avanzando

---
+++ app/src/main/java/asteroides/example/org/asteroides/Juego.java
@@ -0,0 +1,15 @@
+package asteroides.example.org.asteroides;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Vespertino on 22/11/2017.
+ */
+
+public class Juego extends Activity{
+    @Override public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.juego);
+}}
+
