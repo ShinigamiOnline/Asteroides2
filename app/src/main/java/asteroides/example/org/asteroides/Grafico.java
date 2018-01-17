a3e61f74dd1601b1b1e4d61384300075adea64ca
ShinigamiOnline <denshaotoko@outlook.com>
2018-01-17 12:41:36 +0100

    commit home

--- app/src/main/java/asteroides/example/org/asteroides/Grafico.java
+++ app/src/main/java/asteroides/example/org/asteroides/Grafico.java
@@ -10,84 +10,70 @@ import android.view.View;
 
 public class Grafico {
     private Drawable drawable;   //Imagen que dibujaremos
-
-    private double posX, posY;   //Posición
-
-    private double incX, incY;   //Velocidad desplazamiento
-
-    private int angulo, rotacion;//Ángulo y velocidad rotación
-
+    private int cenX, cenY;   //Posición
     private int ancho, alto;     //Dimensiones de la imagen
-
+    private double incX,incY;
+    private double angulo,rotacion;
     private int radioColision;   //Para determinar colisión
-
-    //Donde dibujamos el gráfico (usada en view.ivalidate)
-
+    private int xAnterior,yAnterior;
+    private int radioInval;
     private View view;
 
     // Para determinar el espacio a borrar (view.ivalidate)
-
     public static final int MAX_VELOCIDAD = 20;
 
     public Grafico(View view, Drawable drawable){
 
         this.view = view;
-
         this.drawable = drawable;
-
         ancho = drawable.getIntrinsicWidth();
-
         alto = drawable.getIntrinsicHeight();
-
         radioColision = (alto+ancho)/4;
+        radioInval =(int)Math.hypot(ancho/2,alto/2);
 
     }
     public void dibujaGrafico(Canvas canvas){
 
+        int x=cenX-ancho/2;
+
+        int y=cenY - alto/2;
+        drawable.setBounds(x,y,x+ancho,y+alto);
         canvas.save();
-
-        int x=(int) (posX+ancho/2);
-
-        int y=(int) (posY+alto/2);
-
-        canvas.rotate((float) angulo,(float) x,(float) y);
-
-        drawable.setBounds((int)posX, (int)posY,
-                (int)posX+ancho, (int)posY+alto);
-
+        canvas.rotate((float) angulo,cenX,cenY);
         drawable.draw(canvas);
-
         canvas.restore();
-
-        int rInval = (int) Math.hypot(ancho,alto)/2 + MAX_VELOCIDAD;
-
-        view.invalidate(x-rInval, y-rInval, x+rInval, y+rInval);
-
+        view.invalidate(cenX-radioInval,cenY-radioInval,cenX+radioInval,cenY+radioInval);
+        view.invalidate(xAnterior-radioInval,yAnterior-radioInval,xAnterior+radioInval,yAnterior+radioInval);
+        xAnterior = cenX;
+        yAnterior = cenY;
     }
 
     public void incrementaPos(double factor){
 
-        posX+=incX * factor;
+        cenX+=cenX * factor;
+        cenY+=cenY * factor;
+        angulo += rotacion * factor; //Actualizamos ángulo
 
         // Si salimos de la pantalla, corregimos posición
 
-        if(posX<-ancho/2) {posX=view.getWidth()-ancho/2;}
+        if(cenX<0)
+            cenX=view.getWidth();
 
-        if(posX>view.getWidth()-ancho/2) {posX=-ancho/2;}
+        if(cenX<view.getWidth())
+            cenX=0;
 
-        posY+=incY * factor;
+        if(cenY < 0)
+            cenY = view.getHeight();
 
-        if(posY<-alto/2) {posY=view.getHeight()-alto/2;}
+        if(cenY>view.getHeight())
+            cenY =0;
 
-        if(posY>view.getHeight()-alto/2) {posY=-alto/2;}
-
-        angulo += rotacion * factor; //Actualizamos ángulo
 
     }
 
     public double distancia(Grafico g) {
 
-        return Math.hypot(posX-g.posX, posY-g.posY);
+        return Math.hypot(cenX-g.cenX, cenY-g.cenY);
 
     }
 
@@ -124,14 +110,6 @@ public class Grafico {
         return alto;
     }
 
-    public void setPosY(double posY) {
-        this.posY = posY;
-    }
-
-    public void setPosX(double posX) {
-        this.posX = posX;
-    }
-
     public double getAngulo() {
         return  angulo;
     }
@@ -143,4 +121,30 @@ public class Grafico {
     public double getIncY() {
         return incY;
     }
+
+    public Object getCenX() {
+        return cenX;
+    }
+
+
+    public Object getCenY() {
+        return cenY;
+    }
+
+    public void setCenX(int cenX) {
+        this.cenX = cenX;
+    }
+
+    public void setCenY(int cenY) {
+        this.cenY = cenY;
+    }
+
+    public void setAncho(int ancho) {
+        this.ancho = ancho;
+    }
+
+    public void setAlto(int alto) {
+        this.alto = alto;
+    }
+
 }
--- app/src/main/java/asteroides/example/org/asteroides/VistaJuego.java
+++ app/src/main/java/asteroides/example/org/asteroides/VistaJuego.java
@@ -9,11 +9,19 @@ import android.graphics.Path;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.ShapeDrawable;
 import android.graphics.drawable.shapes.PathShape;
+import android.graphics.drawable.shapes.RectShape;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
 import android.view.View;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.Vector;
@@ -22,7 +30,7 @@ import java.util.Vector;
  * Created by Vespertino on 22/11/2017.
  */
 
-public class VistaJuego extends View {
+public class VistaJuego extends View implements SensorEventListener {
     private static final double MAX_VELOCIDAD_NAVE = 80;
 
     // THREAD Y TIEMPO
@@ -42,7 +50,7 @@ public class VistaJuego extends View {
     private static final int PASO_GIRO_NAVE = 5;
     private static final float PASO_ACELERACION_NAVE = 0.5f;
 
-    // //// ASTEROIDES //////
+    /////// ASTEROIDES //////
 
     private Vector<Grafico> asteroides; // Vector con los Asteroides
 
@@ -50,16 +58,34 @@ public class VistaJuego extends View {
 
     private int numFragmentos = 3; // Fragmentos en que se divide
 
+    ////Pantalla tactil ////
+    private float mX = 0, mY = 0;
+    private boolean disparo = false;
+
+    //Para los sensores
+    private boolean hayValorInicial;
+    private float valorInicial;
+
+    ////MISIL////////
+    private Grafico misil;
+    private Vector<Grafico> misiles; // Vector con los Asteroides
+
+    private static int PASO_VELOCIDAD_MISIL = 12;
+
+    private boolean misilActivo = false;
+
+    private int tiempoMisil;
+
 
     public VistaJuego(Context context, AttributeSet attrs) {
         super(context, attrs);
 
         Drawable drawableNave, drawableAsteroide, drawableMisil;
 
-       // drawableAsteroide = context.getResources().getDrawable(R.drawable.asteroide1);
+        // drawableAsteroide = context.getResources().getDrawable(R.drawable.asteroide1);
 
         SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(getContext());
-        if(pref.getString("graficos","1").equals("0")) {
+        if (pref.getString("graficos", "1").equals("0")) {
             Path pathAteroide = new Path();
             pathAteroide.moveTo((float) 0.3, (float) 0.0);
             pathAteroide.lineTo((float) 0.6, (float) 0.0);
@@ -80,34 +106,51 @@ public class VistaJuego extends View {
             dAsteroide.setIntrinsicHeight(50);
             drawableAsteroide = dAsteroide;
             setBackgroundColor(Color.BLACK);
-            setLayerType(View.LAYER_TYPE_SOFTWARE,null);
-        }else{
-            drawableAsteroide = context.getResources().getDrawable(R.drawable.asteroide1);
-            setLayerType(View.LAYER_TYPE_HARDWARE,null);
+            setLayerType(View.LAYER_TYPE_SOFTWARE, null);
 
+            ShapeDrawable dMisil = new ShapeDrawable(new RectShape());
+            dMisil.getPaint().setColor(Color.WHITE);
+            dMisil.getPaint().setStyle(Paint.Style.STROKE);
+            dMisil.setIntrinsicWidth(15);
+            dMisil.setIntrinsicHeight(3);
+
+            drawableMisil = dMisil;
+
+        } else {
+            drawableAsteroide = context.getResources().getDrawable(R.drawable.asteroide1);
+            setLayerType(View.LAYER_TYPE_HARDWARE, null);
         }
 
         drawableNave = context.getResources().getDrawable(R.drawable.nave);
-
         asteroides = new Vector<Grafico>();
         nave = new Grafico(this, drawableNave);
+
+        drawableMisil = context.getResources().getDrawable(R.drawable.misil1);
+        misil = new Grafico(this, drawableMisil);
 
         for (int i = 0; i < numAsteroides; i++) {
 
             Grafico asteroide = new Grafico(this, drawableAsteroide);
 
             asteroide.setIncY(Math.random() * 4 - 2);
-
             asteroide.setIncX(Math.random() * 4 - 2);
-
             asteroide.setAngulo((int) (Math.random() * 360));
-
             asteroide.setRotacion((int) (Math.random() * 8 - 4));
-
             asteroides.add(asteroide);
 
         }
 
+        SensorManager mSensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
+
+        List<Sensor> listSensors = mSensorManager.getSensorList(
+                Sensor.TYPE_ORIENTATION);
+
+        if (!listSensors.isEmpty()) {
+            Sensor orientationSensor = listSensors.get(0);
+            mSensorManager.registerListener(this, orientationSensor,
+                    SensorManager.SENSOR_DELAY_GAME);
+
+        }
 
     }
 
@@ -120,14 +163,14 @@ public class VistaJuego extends View {
 
         for (Grafico asteroide : asteroides) {
             do {
-                asteroide.setPosX((int) (Math.random() * ancho));
+                asteroide.setIncX((int) (Math.random() * ancho));
 
-                asteroide.setPosY((int) (Math.random() * alto));
-            }while(asteroide.distancia(nave) < (ancho+alto)/5);
+                asteroide.setIncY((int) (Math.random() * alto));
+            } while (asteroide.distancia(nave) < (ancho + alto) / 5);
         }
 
-        nave.setPosX(this.getWidth() / 2);
-        nave.setPosY(this.getHeight() / 2);
+        nave.setIncX(this.getWidth() / 2);
+        nave.setIncY(this.getHeight() / 2);
 
 
         ultimoProceso = System.currentTimeMillis();
@@ -144,6 +187,10 @@ public class VistaJuego extends View {
             asteroide.dibujaGrafico(canvas);
         }
         nave.dibujaGrafico(canvas);
+
+        if (misilActivo) {
+            misil.dibujaGrafico(canvas);
+        }
     }
 
 
@@ -170,9 +217,46 @@ public class VistaJuego extends View {
         for (Grafico asteroide : asteroides) {
             asteroide.incrementaPos(factorMov);
         }
+        // Actualizamos posición de misil
 
+        if (misilActivo) {
+            misil.incrementaPos(factorMov);
+            tiempoMisil -= factorMov;
+            if (tiempoMisil < 0) {
+                misilActivo = false;
+            } else {
+                for (int i = 0; i < asteroides.size(); i++)
+                    if (misil.verificaColision(asteroides.elementAt(i))) {
+                        destruyeAsteroide(i);
+                        break;
+
+                    }
+
+            }
+
+        }
 
     }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        hayValorInicial = false;
+        valorInicial = 0;
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        float valor = event.values[1];
+
+        if (!hayValorInicial) {
+            valorInicial = valor;
+            hayValorInicial = true;
+
+        }
+
+        giroNave = (int) (valor - valorInicial) / 3;
+    }
+
 
     public class ThreadJuego extends Thread {
         @Override
@@ -181,6 +265,134 @@ public class VistaJuego extends View {
                 actualizaFisica();
             }
         }
+
+    }
+
+    @Override
+    public boolean onKeyDown(int codigoTecla, KeyEvent evento) {
+
+        super.onKeyDown(codigoTecla, evento);
+
+        // Suponemos que vamos a procesar la pulsación
+
+        boolean procesada = true;
+
+        switch (codigoTecla) {
+
+            case KeyEvent.KEYCODE_DPAD_UP:
+                aceleracionNave = +PASO_ACELERACION_NAVE;
+                break;
+            case KeyEvent.KEYCODE_DPAD_LEFT:
+                giroNave = -PASO_GIRO_NAVE;
+                break;
+            case KeyEvent.KEYCODE_DPAD_RIGHT:
+                giroNave = +PASO_GIRO_NAVE;
+                break;
+            case KeyEvent.KEYCODE_DPAD_CENTER:
+            case KeyEvent.KEYCODE_ENTER:
+                ActivaMisil();
+                break;
+
+            default:
+
+                // Si estamos aquí, no hay pulsación que nos interese
+                procesada = false;
+                break;
+        }
+        return procesada;
+
+    }
+
+    @Override
+    public boolean onKeyUp(int codigoTecla, KeyEvent evento) {
+
+        super.onKeyUp(codigoTecla, evento);
+
+        // Suponemos que vamos a procesar la pulsación
+
+        boolean procesada = true;
+
+        switch (codigoTecla) {
+
+            case KeyEvent.KEYCODE_DPAD_UP:
+                aceleracionNave = 0;
+                break;
+
+            case KeyEvent.KEYCODE_DPAD_LEFT:
+
+            case KeyEvent.KEYCODE_DPAD_RIGHT:
+                giroNave = 0;
+                break;
+
+            default:
+                // Si estamos aquí, no hay pulsación que nos interese
+                procesada = false;
+                break;
+
+        }
+        return procesada;
+
+    }
+
+    /*La nave con la pantalla tactil*/
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        super.onTouchEvent(event);
+        float x = event.getX();
+        float y = event.getY();
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                disparo = true;
+                break;
+
+            case MotionEvent.ACTION_MOVE:
+                float dx = Math.abs(x - mX);
+                float dy = Math.abs(y - mY);
+                if (dy < 6 && dx > 6) {
+                    giroNave = Math.round((x - mX) / 2);
+                    disparo = false;
+
+                } else if (dx < 6 && dy > 6) {
+                    aceleracionNave = Math.round((mY - y) / 25);
+                    disparo = false;
+
+                }
+                break;
+
+            case MotionEvent.ACTION_UP:
+                giroNave = 0;
+                aceleracionNave = 0;
+                if (disparo) {
+                    ActivaMisil();
+                }
+                break;
+
+        }
+
+        mX = x;
+        mY = y;
+
+        return true;
+
+    }
+
+    private void destruyeAsteroide(int i) {
+
+        asteroides.remove(i);
+        misilActivo = false;
+    }
+
+    private void ActivaMisil() {
+
+        misil.setCenX((Integer) nave.getCenX());
+        misil.setCenY((Integer) nave.getCenY());
+        misil.setAngulo((int) nave.getAngulo());
+        misil.setIncX(Math.cos(Math.toRadians(misil.getAngulo())) * PASO_VELOCIDAD_MISIL);
+        misil.setIncY(Math.sin(Math.toRadians(misil.getAngulo())) * PASO_VELOCIDAD_MISIL);
+        tiempoMisil = (int) Math.min(this.getWidth() / Math.abs(misil.getIncX()), this.getHeight() / Math.abs(misil.getIncY())) - 2;
+        misilActivo = true;
 
     }
 
